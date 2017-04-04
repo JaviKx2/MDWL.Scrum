f05b614ab0e4d784f24ba0e729b24504449e9ead
JaviKx2 <javier.reyes.mangas@alumnos.upm.es>
2017-04-04 12:59:29 +0200

    First commit

---
+++ src/main/java/api/Uris.java
@@ -0,0 +1,9 @@
+package api;
+
+public class Uris {
+
+    public static final String SERVLET_MAP = "/api";
+
+    public static final String VERSION = "/v0";
+
+}
---
+++ src/main/java/config/PackageNames.java
@@ -0,0 +1,14 @@
+package config;
+
+public class PackageNames {
+
+    public static final String PROPERTIES = "classpath:META-INF/application.properties";
+
+    public static final String ENTITIES = "entities";
+
+    public static final String DAOS = "daos";
+
+    public static final String API = "api";
+
+    public static final String SERVICES= "services";
+}
