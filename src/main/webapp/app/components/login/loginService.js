149e412143489325e3675ac015755f17601ec21c
JacoboCL <j.conejol@alumnos.upm.es>
2017-04-22 12:31:25 +0200

    Refactor to delete repeated code.

--- src/main/webapp/app/components/login/loginService.js
+++ src/main/webapp/app/components/login/loginService.js
@@ -1,32 +1,16 @@
-booking.service('loginService', function ($http, $q, $window) {
-   "use strict"; 
-   
-   	const urlBase = "http://localhost:8080/Booking.1.0.0-SNAPSHOT/api/v0";
-   		
-	this.request = config => {
-	      let deferred = $q.defer();
-	      $http(config).then(response => {
-	    	  if (response.data.token){ 
-	    		  $window.localStorage['spring-token'] = response.data.token;
-		          $http.defaults.headers.common['x-access-token'] = response.data.token;
-		      }
-	    	  deferred.resolve(response.data);
-	      }, response => {
-	    	  deferred.reject(`Error `);
-	      });
-	      return deferred.promise;	   
-	}	
+booking.service('loginService', function (API_BASE_URL, BookingFactory) {
+   "use strict";
    
 	this.login = loginData => {
 		  let config = {
 		 	 method: 'POST',
-		 	 url: `${urlBase}/login`,
+		 	 url: `${API_BASE_URL}/login`,
 		 	 data: {
 		 		 email: loginData.email,
 		 		 password: loginData.password
 		 	 }
 		  };
-	      return this.request(config);
+	      return BookingFactory.request(config);
 	}
 
 });
